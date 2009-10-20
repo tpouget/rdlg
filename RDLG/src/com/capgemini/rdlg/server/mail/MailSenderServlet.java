59687de516afa038c0a35063a0e6eefdccf3ed21
tpouget <tpouget@a2a08a16-87e6-11de-b610-71a7c637a048>
2009-10-20 23:41:16 +0000

    Added CRON Service to send mail each working day @ 9h45

---
+++ RDLG/src/com/capgemini/rdlg/server/mail/MailSenderServlet.java
@@ -0,0 +1,50 @@
+package com.capgemini.rdlg.server.mail;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class MailSenderServlet extends HttpServlet { 
+	private static Logger log = Logger.getLogger(MailSenderServlet.class.getName());
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {		
+		
+		String header = req.getHeader("X-AppEngine-Cron");
+		if (Boolean.parseBoolean(header)){
+			try {
+				Properties props = new Properties();
+		        Session session = Session.getDefaultInstance(props, null);
+				
+	            Message msg = new MimeMessage(session);
+	            msg.setFrom(new InternetAddress("tpouget@gmail.com", "RDLG"));
+	            msg.addRecipient(Message.RecipientType.TO,
+	                             new InternetAddress("tpouget@gmail.com"));
+	            msg.addRecipient(Message.RecipientType.CC,
+	                    new InternetAddress("sebastien.etter@gmail.com"));
+	            msg.setSubject("[RDLG] Test Cron Service");
+	            msg.setText("RDLG : bon faudrait peut-être penser à me terminer !!!!");
+	            Transport.send(msg);
+	        } catch (AddressException e) {
+	        	log.warning(e.getMessage());
+	        } catch (MessagingException e) {
+	        	log.warning(e.getMessage());
+	        }
+		} else
+			log.warning("Pas de Header X-AppEngine-Cron");
+	}
+}
